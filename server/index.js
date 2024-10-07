1a098bde9fcf9bf47b16e5b71e549d58a3116518
Eric L <eric.low@gmail.com>
2024-10-07 11:22:15 -0700

    axios tested and works

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(cors());
 
@@ -12,4 +13,12 @@ app.listen(8080, () => {
 
 app.get('/', (req, res) => {
     res.send('Hello from our server!')
+
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => {
+        console.log('Data fetched successfully:', response.data);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 })
