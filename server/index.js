bba1b451bde399a91641bfe843992117942e1b40
Eric L <eric.low@gmail.com>
2024-10-06 23:44:33 -0700

    basic react ui and button with API call complete

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+console.log(app)
+
+app.listen(8080, () => {
+      console.log('server listening on port 8080')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello from our server!')
+})
