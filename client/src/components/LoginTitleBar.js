242b36293d2b82a64aaa9598ec04fde7943f6ebf
Eric L <eric.low@gmail.com>
2024-10-10 15:05:57 -0700

    created a title bar for login screen

---
+++ client/src/components/LoginTitleBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const LoginTitleBar = () => {
+    return (
+            <div className="bg-primary text-white p-3 fixed-top">
+            <div className="container d-flex justify-content-between align-items-center">
+            <h1 className="m-0">ResFrac Weather</h1>
+            <div>
+            </div>
+            </div>
+            </div>
+    )};
+
+export default LoginTitleBar
